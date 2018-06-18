c77a7f781a270a48342b7c8b3ef568bd528cd36c
Pascalis Maschke <Pascalis@DEV-054.local>
2018-06-18 14:54:19 +0200

    possibly corrected bundle.js reference in server.js

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ var path = require("path");
 var app = express();
 // var routes = require("/index");
 
-app.use("/static", express.static(path.join(__dirname, "dist")));
+// app.use("/static", express.static(path.join(__dirname, "dist"))); // correct one
+
+app.use("/static", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/bundle.js"));
+});
+
+console.log(path.join(__dirname + "/dist/bundle.js"));
+
+// console.log(app.use("/static", express.static(path.join(__dirname, "dist"))));
 
 app.use("/", function(req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
